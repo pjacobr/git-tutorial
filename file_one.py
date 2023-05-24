0d50393f5912b162f75a5cb5b66f1699694f4051
Jacob Pettingill <jacobp1794@gmail.com>
2023-05-24 08:41:00 -0600

    add base files

---
+++ file_one.py
@@ -0,0 +1,6 @@
+def hello_world(name: str):
+    print("Hello, " + name + "!")
+
+
+if __name__ == "__main__":
+    hello_world("John")
---
+++ file_two.py
@@ -0,0 +1,6 @@
+def add(num_one, num_two):
+    return num_one + num_two
+
+
+if __name__ == "__main__":
+    add(1, 2)
